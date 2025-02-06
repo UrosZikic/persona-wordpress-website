f1e06c566ae75216613dc7962eb729f5fe1f9b74
UrosZikic <uroszikic1512988@gmail.com>
2025-02-06 20:11:23 +0100

    style changes

--- page-contact.php
+++ page-contact.php
@@ -3,13 +3,12 @@
   <div id="contact_id">
     <div class="contact-container reasons_why_disappear">
       <div name="contact-form" class="form-container">
-        <h2>KONTAKTIRAJTE
-          NAS</h2>
-        <p>Popunite formu i neko će Vam se javiti u narednih nekoliko sati</p>
+        <h2>Contact Me</h2>
+        <p>Fill in the form and I'll contact you</p>
         <p class="form-notification"></p>
         <div class="name_mail_container">
           <div>
-            <label for="from_name">Ime</label>
+            <label for="from_name">Name</label>
             <input id="from_name" name="from_name" type="text" required />
           </div>
           <div>
@@ -17,9 +16,9 @@
             <input type="email" name="email" id="email" required />
           </div>
         </div>
-        <label for="message">Poruka</label>
+        <label for="message">Message</label>
         <textarea name="message" id="message" cols="30" rows="6"></textarea>
-        <button class="button-form--submit" onclick="sendMail();">Pošalji</button>
+        <button class="button-form--submit" onclick="sendMail();">Send</button>
       </div>
       <!-- <img src="../assets/img/2.webp" alt="teacher"> -->
     </div>
